1a518dcfd3143d0cd4a1de9b19e838299e6e4e5a
deemaSah <deemasahyoni@gmail.com>
2022-04-10 10:54:31 +0300

    version 1.0

--- HOG.py
+++ HOG.py
@@ -30,14 +30,49 @@ for (i, imagePath) in enumerate(imagePaths):
     image = cv2.imread(imagePath)
     image = imutils.resize(image, width=600)
     (h, w) = image.shape[:2]
+    cv2.imwrite("cut.jpg", image)
 
 
-# construct a blob from the image
+
+    # preprocess before passing it through our deep neural network for classification.
     imageBlob = cv2.dnn.blobFromImage(
         cv2.resize(image, (300, 300)), 1.0, (300, 300),
         (104.0, 177.0, 123.0), swapRB=False, crop=False)
     # We assumed that the average pixel values of
     # the three layers are (123.0 , 177.0 , 104.0) because the Caffe model is a pre-trained model on the
     # ImageNet training dataset
+
     caffe_model.setInput(imageBlob)
+
     detections = caffe_model.forward()
+   # print("detections")
+#    print(detections[0, 0, :, 2])
+
+    # Processing detected faces
+    # ensure at least one face was found
+    if len(detections) > 0:
+        i = np.argmax(detections[0, 0, :, 2])
+        #print("i")
+        #print(i)
+        confidence = detections[0, 0, i, 2]
+        if confidence > Confidence:
+            box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+            (startX, startY, endX, endY) = box.astype("int")
+            face = image[startY:endY, startX:endX]
+            (fH, fW) = face.shape[:2]
+            if fW < 20 or fH < 20:
+                continue
+
+
+            # quantification of the face
+            faceBlob = cv2.dnn.blobFromImage(face, 1.0 / 255,
+                (96, 96), (0, 0, 0), swapRB=True, crop=False)
+            troch_model.setInput(faceBlob)
+            vec = troch_model.forward()
+            trainingNames.append(name)
+            trainingEmbeddings.append(vec.flatten())
+            numOfFaces += 1
+data = {"embeddings": trainingEmbeddings, "names": trainingNames}
+f = open(embeddings, "wb")
+f.write(pickle.dumps(data))
+f.close()
